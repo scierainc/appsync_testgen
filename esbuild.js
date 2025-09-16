366ddc5ed1c7c78d2e142e58085fbdcd5b1e83b3
Rajesh Soundararajan <rajeshs@sciera.com>
2025-09-16 16:05:44 +0530

    Added logic to package the extension

---
+++ esbuild.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+const esbuild = require('esbuild');
+
+const production = process.argv.includes('--production');
+const watch = process.argv.includes('--watch');
+
+const esbuildProblemMatcherPlugin = {
+  name: 'esbuild-problem-matcher',
+  setup(build) {
+    build.onStart(() => console.log('[watch] build started'));
+    build.onEnd((result) => {
+      result.errors?.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        if (!location) return;
+        console.error(`    ${location.file}:${location.line}:${location.column}:`);
+      });
+      console.log('[watch] build finished');
+    });
+  },
+};
+
+async function main() {
+  const ctx = await esbuild.context({
+    entryPoints: ['src/extension.ts'],
+    bundle: true,
+    platform: 'node',
+    format: 'cjs',
+    target: ['node16'],
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    outfile: 'dist/extension.js',
+    external: ['vscode'],
+    logLevel: 'warning',
+    plugins: [esbuildProblemMatcherPlugin],
+  });
+
+  if (watch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
